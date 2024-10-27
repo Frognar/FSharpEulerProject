77f4a4c753d283d4c248695fdb1799de18b8f978
frognar <sebastian.przyszlak@gmail.com>
2024-10-27 20:12:59 +0100

    test all one-digit numbers

--- ProjectEuler.Tests/LargestPalindromeProductTests.fs
+++ ProjectEuler.Tests/LargestPalindromeProductTests.fs
@@ -3,5 +3,5 @@ module LargestPalindromeProductTests
 open Xunit
 
 [<Fact>]
-let ``isPalindrome of 1 is true`` () =
-    Assert.True(ProjectEuler.isPalindrome 1)
+let ``isPalindrome of any one-digit number is true`` () =
+    [0..9] |> List.iter (fun x -> Assert.True(ProjectEuler.isPalindrome x))
