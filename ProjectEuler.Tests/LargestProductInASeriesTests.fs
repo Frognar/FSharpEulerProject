073965ac4761e01d6b186aeb3b53cf32d7e770bb
frognar <sebastian.przyszlak@gmail.com>
2024-10-31 07:15:49 +0100

    "123" split into substrings of size 2 should be ["12"; "23"]

--- ProjectEuler.Tests/LargestProductInASeriesTests.fs
+++ ProjectEuler.Tests/LargestProductInASeriesTests.fs
@@ -17,3 +17,7 @@ let ``"12" split into substrings of size 1 should be ["1"; "2"]`` () =
 [<Fact>]
 let ``"12" split into substrings of size 2 should be ["12"]`` () =
     Assert.StrictEqual(["12"], ProjectEuler.splitIntoSubstrings 2 "12")
+
+[<Fact>]
+let ``"123" split into substrings of size 2 should be ["12"; "23"]`` () =
+    Assert.StrictEqual(["12"; "23"], ProjectEuler.splitIntoSubstrings 2 "123")
