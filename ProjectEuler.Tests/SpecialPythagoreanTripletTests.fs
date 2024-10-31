255d8921474b4e6b24494f083e0d65a3ca229eff
frognar <sebastian.przyszlak@gmail.com>
2024-10-31 11:23:47 +0100

    Pythagorean triplet for witch a + b + c = 1000 is [[200; 375; 425]]

--- ProjectEuler.Tests/SpecialPythagoreanTripletTests.fs
+++ ProjectEuler.Tests/SpecialPythagoreanTripletTests.fs
@@ -18,3 +18,7 @@ let ``all a < b < c that sum up to 8 are [[1;2;5]; [1;3;4]]`` () =
 let ``all a < b < c that sum up to 9 are [[1;2;6]; [1;3;5]; [2;3;4]]`` () =
     Assert.StrictEqual([[1; 2; 6]; [1; 3; 5]; [2; 3; 4]], ProjectEuler.threeSumCombinations 9)
 
+[<Fact>]
+let ``Pythagorean triplet for witch a + b + c = 1000 is [[200; 375; 425]]`` () =
+    Assert.StrictEqual([[200; 375; 425]], ProjectEuler.threeSumCombinations 1000
+                       |> List.filter (fun x -> x[0] * x[0] + x[1] * x[1] = x[2] * x[2]))
