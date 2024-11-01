edb68220e91a53a4b1011085b09ddbc8f3af0253
frognar <sebastian.przyszlak@gmail.com>
2024-11-01 08:28:17 +0100

    primes up to 100 are [2; 3; 5; 7; 11; 13; 17; 19; 23; 29; 31; 37; 41; 43; 47; 53; 59; 61; 67; 71; 73; 79; 83; 89; 97]

--- ProjectEuler.Tests/SummationOfPrimesTests.fs
+++ ProjectEuler.Tests/SummationOfPrimesTests.fs
@@ -18,3 +18,8 @@ let ``primes up to 3 are [2; 3]`` () =
 let ``primes up to 4 are [2; 3]`` () =
     Assert.StrictEqual([2; 3], ProjectEuler.primesUpTo 4)
 
+[<Fact>]
+let ``primes up to 100 are [2; 3; 5; 7; 11; 13; 17; 19; 23; 29; 31; 37; 41; 43; 47; 53; 59; 61; 67; 71; 73; 79; 83; 89; 97]`` () =
+    let expected = [2; 3; 5; 7; 11; 13; 17; 19; 23; 29; 31; 37; 41; 43; 47; 53; 59; 61; 67; 71; 73; 79; 83; 89; 97]
+    Assert.StrictEqual(expected, ProjectEuler.primesUpTo 100)
+
