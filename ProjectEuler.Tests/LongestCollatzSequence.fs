c227a47d6a90fb7be0936bffec646e68b34551f6
frognar <sebastian.przyszlak@gmail.com>
2024-11-05 06:39:39 +0100

    next Collatz number after 40 is 20

--- ProjectEuler.Tests/LongestCollatzSequence.fs
+++ ProjectEuler.Tests/LongestCollatzSequence.fs
@@ -5,3 +5,7 @@ open Xunit
 [<Fact>]
 let ``next Collatz number after 13 is 40`` () =
     Assert.Equal(40, ProjectEuler.nextCollatz 13)
+
+[<Fact>]
+let ``next Collatz number after 40 is 20`` () =
+    Assert.Equal(20, ProjectEuler.nextCollatz 40)
