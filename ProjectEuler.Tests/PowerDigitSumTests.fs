60f856ea646344f750fa0fa39b7a0de80eeaf4cf
frognar <sebastian.przyszlak@gmail.com>
2024-11-06 13:47:04 +0100

    sum of digits of 2^1 is 2

--- ProjectEuler.Tests/PowerDigitSumTests.fs
+++ ProjectEuler.Tests/PowerDigitSumTests.fs
@@ -5,3 +5,7 @@ open Xunit
 [<Fact>]
 let ``sum of digits of 2^0 is 1`` () =
     Assert.Equal(1I, ProjectEuler.powerDigitSum 2 0)
+
+[<Fact>]
+let ``sum of digits of 2^1 is 2`` () =
+    Assert.Equal(2I, ProjectEuler.powerDigitSum 2 1)
