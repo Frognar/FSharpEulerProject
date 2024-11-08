cb0c110bdb7221c648d81f0fc436634c9dead6a2
frognar <sebastian.przyszlak@gmail.com>
2024-11-08 08:38:22 +0100

    numbers 1 to 1000 written out in words would have 21124 letters

--- ProjectEuler.Tests/NumberLetterCountsTests.fs
+++ ProjectEuler.Tests/NumberLetterCountsTests.fs
@@ -2,13 +2,6 @@
 
 open Xunit
 
-//[<Fact>]
-//let ```numbers 1 to 1000 written out in words would have 21124 letters`` () =
-//    Assert.Equal(21124, [ 1..1000 ]
-//                 |> List.map ProjectEuler.read
-//                 |> List.map (_.Replace(" ", "").Replace("-", "").Length)
-//                 |> List.sum)
-
 [<Theory>]
 [<InlineData(1, "one")>]
 [<InlineData(2, "two")>]
@@ -90,3 +83,10 @@ let ``numbers 101 to 999 are read as expected`` number expected =
 [<Fact>]
 let ``1000 is read as one thousand`` () =
     Assert.Equal("one thousand", ProjectEuler.read 1000)
+
+[<Fact>]
+let ``numbers 1 to 1000 written out in words would have 21124 letters`` () =
+    Assert.Equal(21124, [ 1..1000 ]
+                 |> List.map ProjectEuler.read
+                 |> List.map (_.Replace(" ", "").Replace("-", "").Length)
+                 |> List.sum)
