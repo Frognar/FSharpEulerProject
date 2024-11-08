c779d65a478cfa8d22f2893f9016891c70041d4b
frognar <sebastian.przyszlak@gmail.com>
2024-11-08 08:10:38 +0100

    refactor

--- ProjectEuler/Library.fs
+++ ProjectEuler/Library.fs
@@ -244,34 +244,50 @@ let powerDigitSum (n: int) p =
     res |> string |> Seq.map string |> Seq.map int |> Seq.sum
 
 let read (n: int) =
-    match n with
-    | 1 -> "one"
-    | 2 -> "two"
-    | 3 -> "three"
-    | 4 -> "four"
-    | 5 -> "five"
-    | 6 -> "six"
-    | 7 -> "seven"
-    | 8 -> "eight"
-    | 9 -> "nine"
-    | 10 -> "ten"
-    | 11 -> "eleven"
-    | 12 -> "twelve"
-    | 13 -> "thirteen"
-    | 14 -> "fourteen"
-    | 15 -> "fifteen"
-    | 16 -> "sixteen"
-    | 17 -> "seventeen"
-    | 18 -> "eighteen"
-    | 19 -> "nineteen"
-    | 20 -> "twenty"
-    | 30 -> "thirty"
-    | 40 -> "forty"
-    | 50 -> "fifty"
-    | 60 -> "sixty"
-    | 70 -> "seventy"
-    | 80 -> "eighty"
-    | 90 -> "ninety"
+    let readUnit x =
+        match x with
+        | 1 -> "one"
+        | 2 -> "two"
+        | 3 -> "three"
+        | 4 -> "four"
+        | 5 -> "five"
+        | 6 -> "six"
+        | 7 -> "seven"
+        | 8 -> "eight"
+        | 9 -> "nine"
+        | _ -> ""
+
+    let readTeens x =
+        match x with
+        | 10 -> "ten"
+        | 11 -> "eleven"
+        | 12 -> "twelve"
+        | 13 -> "thirteen"
+        | 14 -> "fourteen"
+        | 15 -> "fifteen"
+        | 16 -> "sixteen"
+        | 17 -> "seventeen"
+        | 18 -> "eighteen"
+        | 19 -> "nineteen"
+        | _ -> ""
+
+    let readTens x =
+        match x with
+        | 20 -> "twenty"
+        | 30 -> "thirty"
+        | 40 -> "forty"
+        | 50 -> "fifty"
+        | 60 -> "sixty"
+        | 70 -> "seventy"
+        | 80 -> "eighty"
+        | 90 -> "ninety"
+        | _ -> ""
+    
+    if n < 10 then readUnit n
+    elif n < 20 then readTeens n
+    else readTens n
+
+
 
 let maximumTotal (triangle: int list list) =
     match triangle |> List.rev with
