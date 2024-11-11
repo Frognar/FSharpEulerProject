5d29b2a89db7d4d69e342a65a3ddfd169b63eccf
frognar <sebastian.przyszlak@gmail.com>
2024-11-11 07:07:13 +0100

    the total of all the name scores in file 0022_names.txt is 871198282

--- ProjectEuler.Tests/NameScoresTests.fs
+++ ProjectEuler.Tests/NameScoresTests.fs
@@ -9,3 +9,23 @@ let ``"COLIN" is worth 3 + 15 + 12 + 9 + 14 = 53`` () =
 [<Fact>]
 let ``"MARY" is worth 13 + 1 + 18 + 25 = 57`` () =
     Assert.Equal(57, ProjectEuler.nameScore "MARY")
+
+[<Fact>]
+let ``"MARY" is in file 0022_names.txt`` () =
+    let lines = System.IO.File.ReadAllLines("../../../../resources/0022_names.txt")
+                |> Array.collect (fun (x: string) -> x.Split(',') |> Array.map (_.Replace("\"", "")))
+
+    Assert.Contains("MARY", lines)
+
+[<Fact>]
+let ``the total of all the name scores in file 0022_names.txt is 871198282`` () =
+    let lines = System.IO.File.ReadAllLines("../../../../resources/0022_names.txt")
+                |> Array.collect (fun (x: string) -> x.Split(',') |> Array.map (_.Replace("\"", "")))
+                |> Array.sort
+    
+    Assert.Equal(871198282L, lines
+                     |> Array.map (fun x -> (int64 (ProjectEuler.nameScore x)))
+                     |> Array.indexed
+                     |> Array.map (fun (i, x) -> (int64 i + 1L) * x)
+                     |> Array.sum)
+    
