c5fc53c88bdba3e8f9ed310a40bf5ffe9ec1722c
frognar <sebastian.przyszlak@gmail.com>
2024-11-17 10:47:36 +0100

    product of a and b for quadratic formula n^2 - an + b, maximizing consecutive primes with a in [-999..999] and b in [-1000..1000], is -59231

--- ProjectEuler.Tests/QuadraticPrimesTests.fs
+++ ProjectEuler.Tests/QuadraticPrimesTests.fs
@@ -44,3 +44,18 @@ let ``quadratic formula n^2 - 79n + 1601 produces primes for n = 0..79`` () =
 [<Fact>]
 let ``quadratic formula n^2 - 79n + 1601 produces 80 consecutive primes`` () =
     Assert.Equal(80, ProjectEuler.countConsecutivePrimes -79 1601)
+
+[<Fact>]
+let ``product of a and b for quadratic formula n^2 - an + b, maximizing consecutive primes with a in [-999..999] and b in [-1000..1000], is -59231`` () =
+    let rec loop a b (maxN, maxA, maxB) =
+        if a > 999 then (maxN, maxA, maxB)
+        else if b > 1000 then loop (a + 1) -1000 (maxN, maxA, maxB)
+        else
+            let n = ProjectEuler.countConsecutivePrimes a b
+            if n > maxN then
+                loop a (b + 1) (n, a, b)
+            else
+                loop a (b + 1) (maxN, maxA, maxB)
+    
+    let _, a, b = loop -999 -1000 (0, 0, 0)
+    Assert.Equal(-59231, a * b)
