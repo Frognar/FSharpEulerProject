9fd6f5ea25a91bbc937171ec0d8d9e772614e11f
frognar <sebastian.przyszlak@gmail.com>
2024-11-24 09:05:42 +0100

    the limit for numbers that can be written as the sum of factorial of its digits is 2177280

--- ProjectEuler.Tests/DigitFactorialsTests.fs
+++ ProjectEuler.Tests/DigitFactorialsTests.fs
@@ -17,3 +17,12 @@ let ``3 cannot be written as the sum of factorial of its digits`` () =
 [<Fact>]
 let ``145 can be written as the sum of factorial of its digits`` () =
     Assert.True(ProjectEuler.isFactorialOfItsDigitSum 145)
+
+[<Fact>]
+let ``the limit for numbers that can be written as the sum of factorial of its digits is 2177280`` () =
+    let nf = [1..9] |> List.fold (*) 1
+    Assert.Equal(2177280, Seq.initInfinite (fun i -> i + 1)
+    |> Seq.takeWhile (fun x -> x < (nf * x |> string |> String.length))
+    |> Seq.rev
+    |> Seq.head
+    |> (*) nf)
