148a18e804a826ad5b084723327dd930d2c59776
frognar <sebastian.przyszlak@gmail.com>
2024-11-25 07:43:15 +0100

    there are 55 circular primes below one million

--- ProjectEuler.Tests/CircularPrimesTests.fs
+++ ProjectEuler.Tests/CircularPrimesTests.fs
@@ -9,3 +9,12 @@ let ``145 can be rotated to 451`` () =
 [<Fact>]
 let ``145 can be rotated to 514`` () =
     Assert.Contains(514, ProjectEuler.rotate 145)
+    
+[<Fact>]
+let ``there are 55 circular primes below one million`` () =
+    Assert.Equal(55,
+                 ProjectEuler.primesUpTo 1_000_000
+                 |> List.map (fun x -> (x, ProjectEuler.rotate x))
+                 |> List.filter (fun (_, rotations) -> (rotations |> Seq.forall ProjectEuler.isPrime))
+                 |> List.map fst
+                 |> List.length)
