868a92dd5f303293acca5f7d855b620950bae401
frognar <sebastian.przyszlak@gmail.com>
2024-11-28 07:45:32 +0100

    concatenated products of 192 x [1..3] and 9 x [1..5] are pandigital 1-9

--- ProjectEuler.Tests/PandigitalMultiplesTests.fs
+++ ProjectEuler.Tests/PandigitalMultiplesTests.fs
@@ -9,3 +9,11 @@ let ``concatenated product of 192 and [1..3] is "192384576"`` () =
 [<Fact>]
 let ``concatenated product of 9 and [1..5] is "918273645"`` () =
     Assert.Equal("918273645", ProjectEuler.concatenatedProduct 9 [1..5])
+
+[<Fact>]
+let ``concatenated product of 192 and [1..3] is pandigital 1-9`` () =
+    Assert.True(ProjectEuler.isPandigital 9 (ProjectEuler.concatenatedProduct 192 [1..3]))
+
+[<Fact>]
+let ``concatenated product of 9 and [1..5] is pandigital 1-9`` () =
+    Assert.True(ProjectEuler.isPandigital 9 (ProjectEuler.concatenatedProduct 9 [1..5]))
