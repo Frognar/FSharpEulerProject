d64733416a7cd11d3d590478cb4d2e1e8ee1918c
frognar <sebastian.przyszlak@gmail.com>
2024-12-19 20:46:48 +0100

    minimal difference between two pentagon numbers is 5482660

--- ProjectEuler.Tests/PentagonNumbersTests.fs
+++ ProjectEuler.Tests/PentagonNumbersTests.fs
@@ -29,3 +29,24 @@ let ``5 is pentagon number`` () =
 [<Fact>]
 let ``5482660 is pentagon number`` () =
     Assert.True(ProjectEuler.isPentagonNumber 5482660)
+
+[<Fact>]
+let ``minimal difference between two pentagon numbers is 5482660`` () =
+    let pentagonals = ProjectEuler.pentagonNumbers ()
+                      |> Seq.take 10000
+                      |> Seq.toArray
+
+    let minD = pentagonals
+            |> Array.indexed
+            |> Array.collect (fun (j, pj) ->
+                pentagonals
+                |> Array.skip (j + 1)
+                |> Array.map (fun pk ->
+                    let sum = pj + pk
+                    let diff = pk - pj
+                    (sum, diff)))
+            |> Array.where (fun (s, d) -> ProjectEuler.isPentagonNumber s && ProjectEuler.isPentagonNumber d)
+            |> Array.map snd
+            |> Array.min
+
+    Assert.Equal(5482660, minD)
