ed1eefdce2e3661c4131bf6fccdde1d37bbe9e3f
frognar <sebastian.przyszlak@gmail.com>
2024-12-24 22:21:34 +0100

    permutations returns all unique permutations

--- ProjectEuler.Tests/PrimePermutationsTests.fs
+++ ProjectEuler.Tests/PrimePermutationsTests.fs
@@ -10,3 +10,11 @@ let ``isPrime correctly identifies prime numbers`` () =
     Assert.False(ProjectEuler.isPrime 1)
     Assert.False(ProjectEuler.isPrime 4)
     Assert.False(ProjectEuler.isPrime 1000)
+
+[<Fact>]
+let ``permutations returns all unique permutations`` () =
+    let perms = ProjectEuler.permutations [1; 4; 8; 7] |> Seq.toList
+    Assert.Contains([1; 4; 8; 7], perms)
+    Assert.Contains([4; 8; 1; 7], perms)
+    Assert.Contains([8; 1; 4; 7], perms)
+    Assert.Equal(24, perms |> List.length)
